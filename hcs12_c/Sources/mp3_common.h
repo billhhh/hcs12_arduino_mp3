125561cc8a270139bcccb4add83c3fcc4c60f471
Bill Dufresne <371570273@qq.com>
2016-12-09 22:29:23 -0600

    scoll name ing

---
+++ hcs12_c/Sources/mp3_common.h
@@ -0,0 +1,56 @@
+/**
+ * author: Bill Wang
+ * date: 12/8/2016
+**/
+
+#ifndef _MP3_COMMON_H_
+#define _MP3_COMMON_H_
+
+const unsigned char keypad[4][4] =
+{
+'0','1','2','3',
+'4','5','6','7',
+'8','9','A','B',
+'C','D','E','F'
+};
+
+char songName[16][30] = {
+"1: fate",
+"2: Shock Me into Love",
+"3: lonely northen earth",
+"4: most important dicision",
+"5: Beautiful In White",
+"6: Love Paradise",
+"7: Laputa",
+"8: Nothing's Gonna Change ",
+"9: dream wedding",
+"10: Right Here Waiting",
+"11: Now And Forever",
+"12: I Love You Too Much",
+"13: S of My Heart",
+"14: Kiss The Rain",
+"15: End of the world",
+"16: あなただけ需つめてる"
+};
+
+char scollName[17][45] = {
+"1: fate ",
+"2: Shock Me into Love ",
+"3: lonely northen earth ",
+"4: most important dicision ",
+"5: Beautiful In White ",
+"6: Love Paradise ",
+"7: Laputa ",
+"8: Nothing's Gonna Change The Love For You",
+"9: dream wedding ",
+"10: Right Here Waiting ",
+"11: Now And Forever ",
+"12: I Love You Too Much ",
+"13: S of My Heart ",
+"14: Kiss The Rain ",
+"15: End of the world ",
+"16: あなただけ需つめてる ",
+"Hello, please select "
+};
+
+#endif
